65bc4e15dd7ebb4540b3fc1427e80af08035baeb
Alois Sečkár <ellrohir@seznam.cz>
2023-11-25 14:17:16 +0100

    feat: basic style setup

--- app.vue
+++ app.vue
@@ -1,6 +1,52 @@
 <template>
-  <div>
-    <h1>Elven Ipsum</h1>
-    <div>Lorem ipsum with Elvish words</div>
+  <div class="content">
+    <img
+      src="/logo.webp"
+      class="logo shadow"
+      alt="Elven Ipsum - Lorem ipsum with Elvish words"
+      title="Elven Ipsum - Lorem ipsum with Elvish words"
+    >
+    <div class="box shadow">
+      INPUT
+    </div>
+    <div class="box shadow">
+      OUTPUT
+    </div>
+    <div>
+      &copy; {{ new Date().getFullYear() }}
+      <a href="https://alois-seckar.cz">Alois Sečkár</a>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+useHead({
+  title: 'Elven Ipsum',
+  meta: [
+    { name: 'description', content: 'Lorem ipsum with Elvish words' }
+  ],
+  htmlAttrs: {
+    lang: 'en'
+  }
+})
+</script>
+
+<style>
+.content {
+  margin: 2em;
+  text-align: center;
+}
+.logo {
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+}
+.box {
+  border: 1px solid black;
+  border-radius: 2em;
+  margin: 2em auto;
+}
+.shadow {
+  box-shadow: 0 0 50px 12px black;
+}
+</style>
