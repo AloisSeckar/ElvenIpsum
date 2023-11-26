29b6884f15041d73d6cdbf0e8d44b63dd1975e1b
Alois Sečkár <ellrohir@seznam.cz>
2023-11-26 09:08:38 +0100

    feat: fix input form

--- components/TheInput.vue
+++ components/TheInput.vue
@@ -1,23 +1,27 @@
 <template>
-  <FormKit type="form" :value="input" submit-label="Generate" @submit="$emit('generate', input)">
-    <FormKit type="number" name="paragraphs" label="Paragraphs" />
-    <FormKit type="number" name="minSentences" label="Min sentences in paragraph" />
-    <FormKit type="number" name="maxSentences" label="Max sentences in paragraph" />
-    <FormKit type="number" name="minWords" label="Min words in sentence" />
-    <FormKit type="number" name="maxWords" label="Nax words in sentence" />
+  <FormKit type="form" :value="formData" submit-label="Generate" @submit="handle">
+    <FormKit type="number" number="integer" name="paragraphs" label="Paragraphs" />
+    <FormKit type="number" number="integer" name="minSentences" label="Min sentences in paragraph" />
+    <FormKit type="number" number="integer" name="maxSentences" label="Max sentences in paragraph" />
+    <FormKit type="number" number="integer" name="minWords" label="Min words in sentence" />
+    <FormKit type="number" number="integer" name="maxWords" label="Max words in sentence" />
   </FormKit>
 </template>
 
 <script setup lang="ts">
-defineEmits<{
+const emit = defineEmits<{
   generate: [input: IpsumOptions]
 }>()
 
-const input = ref<IpsumOptions>({
+const formData = ref({
   paragraphs: 5,
   minSentences: 5,
   maxSentences: 10,
   minWords: 5,
   maxWords: 15
 })
+
+function handle (currentData: IpsumOptions) {
+  emit('generate', currentData)
+}
 </script>
