8bd0a8c139261f691a7c6a2f39aaca23d6867c8c
Alois Sečkár <ellrohir@seznam.cz>
2023-11-27 10:53:24 +0100

    feat: copy result to clipboard

--- components/TheOutput.vue
+++ components/TheOutput.vue
@@ -1,11 +1,32 @@
 <template>
-  <div v-for="paragraph in ipsum?.paragraphs" :key="paragraph" class="output">
-    {{ paragraph }}
+  <div>
+    <div v-for="paragraph in ipsum?.paragraphs" :key="paragraph" class="output">
+      {{ paragraph }}
+    </div>
+    <button @click="toClipboard">
+      Copy to clipboard
+    </button>
   </div>
 </template>
 
 <script setup lang="ts">
-defineProps<{
+const props = defineProps<{
   ipsum?: IpsumResults
 }>()
+
+function toClipboard () {
+  let text = ''
+  props.ipsum?.paragraphs.forEach((p) => {
+    if (text) {
+      text += '\n\n'
+    }
+    text += p
+  })
+
+  navigator.clipboard.writeText(text).then(function () {
+    console.log('Text successfully copied to clipboard')
+  }).catch(function (err) {
+    console.error('Error in copying text: ', err)
+  })
+}
 </script>
