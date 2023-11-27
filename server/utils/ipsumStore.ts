96efc88edf0aacc3b7a474391234ff49eec4c33f
Alois Sečkár <ellrohir@seznam.cz>
2023-11-27 09:09:46 +0100

    feat: cache words for 1 hour

--- server/utils/ipsumStore.ts
+++ server/utils/ipsumStore.ts
@@ -4,8 +4,8 @@ import { serverSupabaseClient } from '#supabase/server'
 let elvenDictionary: string[] = []
 
 export async function getElvenDictionary (event: H3Event<EventHandlerRequest>): Promise<string[]> {
-  if (elvenDictionary.length === 0) {
-    console.debug('reloading words from DB')
+  if (elvenDictionary.length === 0 || await cacheExpired()) {
+    console.debug('Fetching dictionary from DB')
     const client = await serverSupabaseClient(event)
     const { data } = await client
       .from('elrh_elven_words')
@@ -14,8 +14,21 @@ export async function getElvenDictionary (event: H3Event<EventHandlerRequest>): 
 
     if (data) {
       elvenDictionary = data.map(w => w.word as string)
+      await useStorage().setItem('lastFetch', new Date().toISOString())
     }
   }
 
   return elvenDictionary
+}
+
+async function cacheExpired () {
+  console.debug('check for cache')
+  const lastFetch = await useStorage().getItem<string>('lastFetch')
+  console.debug('value = ' + lastFetch)
+  if (lastFetch) {
+    const oneHour = 2 * 1000 * 60 * 60
+    const now = new Date()
+    return now.getTime() - new Date(lastFetch).getTime() > oneHour
+  }
+  return true
 }
