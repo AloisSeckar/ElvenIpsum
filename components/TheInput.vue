b91e8186502ec8ffab2666931394d819d643fb1f
Alois Sečkár <ellrohir@seznam.cz>
2023-12-11 20:36:06 +0100

    feat: min and max values in input

--- components/TheInput.vue
+++ components/TheInput.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mx-auto max-w-[1200px]" style="box-shadow: none !important;">
+  <div class="mx-auto max-w-[1100px]" style="box-shadow: none !important;">
     <h1 class="mb-4 text-2xl">
       Customize your Elven ipsum
     </h1>
@@ -11,11 +11,51 @@
       :submit-attrs="cssSubmit"
       @submit="handle"
     >
-      <FormKit type="number" number="integer" name="paragraphs" label="Number of paragraphs" :wrapper-class="cssInput" />
-      <FormKit type="number" number="integer" name="minSentences" label="Min sentences in paragraph" :wrapper-class="cssInput" />
-      <FormKit type="number" number="integer" name="maxSentences" label="Max sentences in paragraph" :wrapper-class="cssInput" />
-      <FormKit type="number" number="integer" name="minWords" label="Min words in sentence" :wrapper-class="cssInput" />
-      <FormKit type="number" number="integer" name="maxWords" label="Max words in sentence" :wrapper-class="cssInput" />
+      <FormKit
+        type="number"
+        number="integer"
+        name="paragraphs"
+        label="Number of paragraphs"
+        :min="1"
+        :max="100"
+        :wrapper-class="cssInput"
+      />
+      <FormKit
+        type="number"
+        number="integer"
+        name="minSentences"
+        label="Min sentences in paragraph"
+        :min="1"
+        :max="20"
+        :wrapper-class="cssInput"
+      />
+      <FormKit
+        type="number"
+        number="integer"
+        name="maxSentences"
+        label="Max sentences in paragraph"
+        :min="1"
+        :max="20"
+        :wrapper-class="cssInput"
+      />
+      <FormKit
+        type="number"
+        number="integer"
+        name="minWords"
+        label="Min words in sentence"
+        :min="1"
+        :max="20"
+        :wrapper-class="cssInput"
+      />
+      <FormKit
+        type="number"
+        number="integer"
+        name="maxWords"
+        label="Max words in sentence"
+        :min="1"
+        :max="50"
+        :wrapper-class="cssInput"
+      />
     </FormKit>
   </div>
 </template>
@@ -25,11 +65,11 @@ const emit = defineEmits<{
   generate: [input: IpsumOptions]
 }>()
 
-const cssForm = 'flex flex-row items-end gap-x-2'
-const cssInput = 'items-center'
+const cssForm = 'mx-auto flex flex-row items-end gap-x-2'
+const cssInput = 'items-center grow'
 const cssSubmit = {
   inputClass: 'px-2 font-bold bg-amber-100',
-  wrapperClass: 'mb-5'
+  wrapperClass: 'mb-5 grow-0'
 }
 
 const formData = ref({
